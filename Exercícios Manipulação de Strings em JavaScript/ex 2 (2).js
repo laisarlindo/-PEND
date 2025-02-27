ff57629d90dbb45c7df2233fc939c52abd0da06d
laisarlindo <159180549+laisarlindo@users.noreply.github.com>
2025-02-27 10:59:57 -0300

    Add files via upload

---
+++ Exercícios Manipulação de Strings em JavaScript/ex 2 (2).js
@@ -0,0 +1,4 @@
+let palavra = "Programação";
+
+console.log(palavra.charAt(0));// Exibe a primeira letra
+console.log(palavra.charAt(10));// Exibe a última letra
---
+++ Exercícios Manipulação de Strings em JavaScript/ex 3 (2).js
@@ -0,0 +1,4 @@
+let mensagem = "Aprendendo JavaScript";
+
+console.log(mensagem.slice(10,21));// Exibe JavaScript
+
