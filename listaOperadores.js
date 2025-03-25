bf08d8599927c2d2fd6a1a2dc724fb46da96a286
laisarlindo <159180549+laisarlindo@users.noreply.github.com>
2025-03-25 15:59:28 -0300

    Add files via upload

---
+++ listaOperadores.js
@@ -0,0 +1,40 @@
+// ex1:
+
+let x = "10";
+console.log(x + "exercico 1"); // saida: 10
+
+// ex2:
+
+let a = 20;
+let b = 5;
+
+console.log(a+b + "exercicio 2 soma"); // saida: 25
+console.log(a-b + "exercicio 2 subtração"); // saida: 15
+console.log(a*b + "exercico 2 multiplicação"); //saida: 100
+console.log(a/b + "exercico 2 divisão"); //saida: 4
+console.log(a%b + "exercicio 2 resto da divisão"); //saida: 0
+
+//ex3:
+
+let num = -15;
+
+console.log(-num + " exercicio 3") //saida: 3
+
+//ex4:
+
+let count = 5;
+
+console.log ( ++count  + " exericio 4 incremento"); //saida: 6
+console.log(--count + " exercicio 4 decremento"); //saida: 5
+
+//ex5:
+
+{
+    let x = 10;
+    console.log(x += 5 + " exercio5");
+    console.log(x -= 3 + " exercicio5");
+    console.log(x *= 2 + " exercico5");
+    console.log(x /= 4 + " exercico5");
+   console.log (x %= 3 + " exercico5");
+
+}
